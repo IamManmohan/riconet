b9a58a574eea606c90134c92bbb25d76770f627d
ashfakh_rivigo <ashfakh.k@rivigo.com>
2017-10-27 09:13:05 +0000

    more tests added

---
+++ core/src/test/java/com/rivigo/riconet/core/test/DepsNotificationServiceTest.java
@@ -0,0 +1,50 @@
+package com.rivigo.riconet.core.test;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rivigo.riconet.core.service.DEPSRecordService;
+import com.rivigo.zoom.common.dto.DEPSNotificationContext;
+import com.rivigo.zoom.common.dto.DEPSNotificationDTO;
+import com.rivigo.zoom.common.model.DEPSRecord;
+import com.rivigo.zoom.common.model.mongo.DEPSNotification;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by ashfakh on 27/10/17.
+ */
+
+@Slf4j
+public class DepsNotificationServiceTest extends TesterBase {
+
+    @Autowired
+    DEPSRecordService depsRecordService;
+
+    @Test
+    public void processNotification()
+    {
+        String str ="[{\"id\":1,\"consignmentId\":1,\"depsType\":\"SHORTAGE\",\"tripId\":1,\"tripType\":\"PRS\",\"taskId\":1,\"reportedById\":50228,\"inboundLocationId\":15,\"depsTaskType\":\"UNLOADING\"},{\"id\":2,\"consignmentId\":1,\"depsType\":\"SHORTAGE\",\"tripId\":1,\"tripType\":\"PRS\",\"taskId\":1,\"reportedById\":50228,\"inboundLocationId\":15,\"depsTaskType\":\"UNLOADING\"}]";
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        DEPSNotificationContext context = null;
+        List<DEPSNotificationDTO> depsRecordList= new ArrayList<>();
+        TypeReference<List<DEPSNotificationDTO>> mapType = new TypeReference<List<DEPSNotificationDTO>>() {};
+        try {
+            depsRecordList = objectMapper.readValue(str, mapType);
+        }catch(IOException e){
+            log.error("mapping failure",e);
+        }
+        context = depsRecordService.getNotificationContext(depsRecordList);
+        List<DEPSNotification> depsNotificationList = depsRecordService.createNotificationData(context);
+        depsRecordService.sendNotifications(depsNotificationList);
+        assertEquals(str,"[{\"id\":1,\"consignmentId\":1,\"depsType\":\"SHORTAGE\",\"tripId\":1,\"tripType\":\"PRS\",\"taskId\":1,\"reportedById\":50228,\"inboundLocationId\":15,\"depsTaskType\":\"UNLOADING\"},{\"id\":2,\"consignmentId\":1,\"depsType\":\"SHORTAGE\",\"tripId\":1,\"tripType\":\"PRS\",\"taskId\":1,\"reportedById\":50228,\"inboundLocationId\":15,\"depsTaskType\":\"UNLOADING\"}]");
+    }
+}
--- notificationConsumer/src/test/java/com/rivigo/riconet/notificationConsumer/test/CreateDepsNotificationTest.java
+++ notificationConsumer/src/test/java/com/rivigo/riconet/notificationConsumer/test/CreateDepsNotificationTest.java
@@ -1,11 +1,9 @@
 package com.rivigo.riconet.notificationConsumer.test;
 
-import com.rivigo.zoom.common.repository.mysql.CityRepository;
+import com.rivigo.riconet.core.test.TesterBase;
 import com.rivigo.riconet.notificationConsumer.depsNotification.DEPSNotificationConsumer;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.rivigo.riconet.core.service.DEPSRecordService;
-import com.rivigo.riconet.core.test.TesterBase;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,12 +12,6 @@ import static org.junit.Assert.assertEquals;
  */
 public class CreateDepsNotificationTest extends TesterBase{
 
-
-    @Autowired
-    CityRepository cityRepository;
-
-    @Autowired
-    DEPSRecordService depsRecordService;
 
     @Autowired
     DEPSNotificationConsumer depsNotificationConsumer;
@@ -32,12 +24,11 @@ public class CreateDepsNotificationTest extends TesterBase{
         assertEquals(str,"[{\"id\":1,\"consignmentId\":1,\"depsType\":\"SHORTAGE\",\"tripId\":1,\"tripType\":\"PRS\",\"taskId\":1,\"reportedById\":50228,\"inboundLocationId\":15,\"depsTaskType\":\"UNLOADING\"},{\"id\":2,\"consignmentId\":1,\"depsType\":\"SHORTAGE\",\"tripId\":1,\"tripType\":\"PRS\",\"taskId\":1,\"reportedById\":50228,\"inboundLocationId\":15,\"depsTaskType\":\"UNLOADING\"}]");
     }
 
-//    @Test
-//    public void processNotificationException()
-//    {
-//        City city=new City();
-//        cityRepository.save(city);
-//        assert city.getId()!= null;
-//    }
+    @Test(expected = Exception.class)
+    public void processNotificationException()
+    {
+        depsNotificationConsumer.processMessage("test");
+    }
+
 
 }
