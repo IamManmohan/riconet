797c91fa1c0c6121cd95b35286e86c55522a00c5
tataradhakrishna <tataradhakrishna6@gmail.com>
2018-08-23 10:05:09 +0530

    hilti test case fixed

--- core/src/test/java/com/rivigo/riconet/core/test/HiltiApiServiceTest.java
+++ core/src/test/java/com/rivigo/riconet/core/test/HiltiApiServiceTest.java
@@ -8,7 +8,6 @@ import com.rivigo.riconet.core.dto.hilti.HiltiRequestDto;
 import com.rivigo.riconet.core.enums.EventName;
 import com.rivigo.riconet.core.service.ConsignmentReadOnlyService;
 import com.rivigo.riconet.core.service.ConsignmentScheduleService;
-import com.rivigo.riconet.core.service.RestClientUtilityService;
 import com.rivigo.riconet.core.service.impl.HiltiApiServiceImpl;
 import com.rivigo.riconet.core.service.impl.RestClientUtilityServiceImpl;
 import com.rivigo.riconet.core.test.Utils.ApiServiceUtils;
@@ -27,15 +26,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Spy;
 import org.springframework.test.util.ReflectionTestUtils;
 
 @Slf4j
-public class HiltiApiServiceTest extends TesterBase {
+public class HiltiApiServiceTest {
 
-  @Autowired private ObjectMapper objectMapper;
+  @Spy private ObjectMapper objectMapper;
 
-  @Autowired private RestClientUtilityServiceImpl restClientUtilityService;
+  @InjectMocks private RestClientUtilityServiceImpl restClientUtilityService;
 
   @Mock private PickupRepository pickupRepository;
 
@@ -58,14 +57,11 @@ public class HiltiApiServiceTest extends TesterBase {
 
   @Before
   public void initMocks() {
-    RestClientUtilityService restClientUtilityServiceSpy = Mockito.spy(restClientUtilityService);
     MockitoAnnotations.initMocks(this);
     ReflectionTestUtils.setField(
         hiltiApiService,
         "hiltiUpdateTransactionsUrl",
         "https://staging.fareye.co/api/v1/update_transactions_status?api_key=VmyY0lEUNrj4eUUn5jqWYMgGjpeeLtDS");
-    ReflectionTestUtils.setField(
-        hiltiApiService, "restClientUtilityService", restClientUtilityServiceSpy);
     ReflectionTestUtils.setField(hiltiApiService, "objectMapper", objectMapper);
     Mockito.when(pickupRepository.findOne(ApiServiceUtils.PICKUP_ID))
         .thenReturn(ApiServiceUtils.getDummyPickup());
@@ -177,6 +173,5 @@ public class HiltiApiServiceTest extends TesterBase {
     addIntransitArrivedAtDestinationEvent();
     addDeliveryOFDEvent();
     addDeliveryDeliveredEvent();
-    sleep(100000);
   }
 }
