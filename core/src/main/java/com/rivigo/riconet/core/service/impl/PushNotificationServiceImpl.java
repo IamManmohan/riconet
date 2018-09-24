3c94abf81e077b1377b7f002a8ebc61973a2c6b9
ashfakh_rivigo <ashfakh.k@rivigo.com>
2018-09-24 14:49:08 +0530

    piush notification tested

--- core/src/main/java/com/rivigo/riconet/core/service/impl/PushNotificationServiceImpl.java
+++ core/src/main/java/com/rivigo/riconet/core/service/impl/PushNotificationServiceImpl.java
@@ -26,8 +26,8 @@ public class PushNotificationServiceImpl implements PushNotificationService {
   // @Value("${firebase.url}")
   private String firebaseUrl = "https://fcm.googleapis.com/fcm/send";
 
-  //    @Value("${firebase.server,key}")
-  private String firebaseServerKey = "AIzaSyBarykZAGZMAVZw5UUKJa9Ozx_mrF1QjHs";
+  //    @Value("${firebase.server.key}")
+  private String firebaseServerKey = "AIzaSyD9E1NeCzE_NpCMA6v4zbhhei64yVxiixw";
 
   @Autowired private RestTemplate riconetRestTemplate;
 
@@ -57,19 +57,19 @@ public class PushNotificationServiceImpl implements PushNotificationService {
   public void send(String message, String firebaseToken) throws IOException {
 
     JSONObject body = new JSONObject();
-    body.put("to", "/topics/" + "topic name");
+    body.put("to", firebaseToken);
     body.put("priority", "high");
 
     JSONObject notification = new JSONObject();
-    notification.put("title", "JSA Notification");
-    notification.put("body", "Happy Message!");
+    notification.put("title", "Unloading In Loading");
+    notification.put("body", "hey! unloading in loading is happening!");
 
-    JSONObject data = new JSONObject();
-    data.put("Key-1", "JSA Data 1");
-    data.put("Key-2", "JSA Data 2");
+//    JSONObject data = new JSONObject();
+//    data.put("Key-1", "JSA Data 1");
+//    data.put("Key-2", "JSA Data 2");
 
     body.put("notification", notification);
-    body.put("data", data);
+   // body.put("data", data);
 
     UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(firebaseUrl);
     URI uri = builder.build().encode().toUri();
