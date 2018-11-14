a06e6aabb8df2044363b4f839bba7510c9232b15
Komal Agarwal <komal.agarwal@partners.rivigo.com>
2018-11-14 14:04:33 +0530

    adding new operator for list

---
+++ core/src/main/java/com/rivigo/riconet/ruleengine/operators/AnyMatchInList.java
@@ -0,0 +1,57 @@
+package com.rivigo.riconet.ruleengine.operators;
+
+import com.rivigo.riconet.ruleengine.BaseType;
+import com.rivigo.riconet.ruleengine.Operation;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+  public class AnyMatchInList  extends Operation {
+    public AnyMatchInList () {
+      super("ANY_MATCH_IN_LIST");
+    }
+
+    public AnyMatchInList copy() {
+      return new AnyMatchInList();
+    }
+
+    @Override
+    public BaseType<Boolean> interpret(Map<String, ?> bindings) {
+
+      BaseType<?> rightBT = this.rightOperand.interpret(bindings);
+      BaseType<?> leftBT = this.leftOperand.interpret(bindings);
+      if (leftBT.getType() == ArrayList.class) {
+        if (((List) leftBT.getValue()).get(0) instanceof Integer) {
+          return new BaseType<>(
+              ((List<Integer>) leftBT.getValue())
+                  .stream()
+                  .anyMatch(v -> new BaseType<>(v, Integer.class).isEquals(rightBT)),
+              Boolean.class);
+        }
+        if (((List) leftBT.getValue()).get(0) instanceof Double) {
+          return new BaseType<>(
+              ((List<Double>) leftBT.getValue())
+                  .stream()
+                  .anyMatch(v -> new BaseType<>(v, Double.class).isEquals(rightBT)),
+              Boolean.class);
+        }
+        if (((List) leftBT.getValue()).get(0) instanceof Boolean) {
+          return new BaseType<>(
+              ((List<Boolean>) leftBT.getValue())
+                  .stream()
+                  .anyMatch(v -> new BaseType<>(v, Boolean.class).isEquals(rightBT)),
+              Boolean.class);
+        }
+        if (((List) leftBT.getValue()).get(0) instanceof String) {
+          return new BaseType<>(
+              ((List<String>) leftBT.getValue())
+                  .stream()
+                  .anyMatch(v -> new BaseType<>(v, String.class).isEquals(rightBT)),
+              Boolean.class);
+        }
+      }
+      return new BaseType<>(Boolean.FALSE,Boolean.class);
+    }
+  }
+
+
--- core/src/main/java/com/rivigo/riconet/ruleengine/operators/Equals.java
+++ core/src/main/java/com/rivigo/riconet/ruleengine/operators/Equals.java
@@ -4,9 +4,10 @@ package com.rivigo.riconet.ruleengine.operators;
 
 import com.rivigo.riconet.ruleengine.BaseType;
 import com.rivigo.riconet.ruleengine.Operation;
-import java.util.List;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Equals extends Operation {
 
   public Equals() {
@@ -20,41 +21,10 @@ public class Equals extends Operation {
 
   @Override
   public BaseType<Boolean> interpret(Map<String, ?> bindings) {
+
     BaseType<?> rightBT = this.rightOperand.interpret(bindings);
 
     BaseType<?> leftBT = this.leftOperand.interpret(bindings);
-
-    if (leftBT.getType() == List.class) {
-      if (((List) leftBT.getValue()).get(0) instanceof Integer) {
-        return new BaseType<>(
-            ((List<Integer>) leftBT.getValue())
-                .stream()
-                .anyMatch(v -> new BaseType<>(v, Integer.class).isEquals(rightBT)),
-            Boolean.class);
-      }
-      if (((List) leftBT.getValue()).get(0) instanceof Double) {
-        return new BaseType<>(
-            ((List<Double>) leftBT.getValue())
-                .stream()
-                .anyMatch(v -> new BaseType<>(v, Double.class).isEquals(rightBT)),
-            Boolean.class);
-      }
-      if (((List) leftBT.getValue()).get(0) instanceof Boolean) {
-        return new BaseType<>(
-            ((List<Boolean>) leftBT.getValue())
-                .stream()
-                .anyMatch(v -> new BaseType<>(v, Boolean.class).isEquals(rightBT)),
-            Boolean.class);
-      }
-      if (((List) leftBT.getValue()).get(0) instanceof String) {
-        return new BaseType<>(
-            ((List<String>) leftBT.getValue())
-                .stream()
-                .anyMatch(v -> new BaseType<>(v, String.class).isEquals(rightBT)),
-            Boolean.class);
-      }
-    }
-
 
     Boolean result = leftBT.isEquals(rightBT);
 
