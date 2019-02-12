4c2d134fdd1b50ad9bb3e0e90127412427485769
ashfakh_rivigo <ashfakh.k@rivigo.com>
2019-02-12 13:41:52 +0530

    committing offsets

--- core/src/main/java/com/rivigo/riconet/core/consumerabstract/ConsumerModel.java
+++ core/src/main/java/com/rivigo/riconet/core/consumerabstract/ConsumerModel.java
@@ -1,16 +1,11 @@
 package com.rivigo.riconet.core.consumerabstract;
 
 import akka.Done;
-import akka.NotUsed;
-import akka.kafka.CommitterSettings;
 import akka.kafka.ConsumerMessage;
 import akka.kafka.ConsumerSettings;
 import akka.kafka.Subscriptions;
-import akka.kafka.javadsl.Committer;
 import akka.kafka.javadsl.Consumer;
 import akka.stream.ActorMaterializer;
-import akka.stream.javadsl.Flow;
-import akka.stream.javadsl.Keep;
 import akka.stream.javadsl.Sink;
 import com.rivigo.zoom.common.model.mongo.ConsumerMessages;
 import com.rivigo.zoom.common.repository.mongo.ConsumerMessagesRepository;
@@ -23,7 +18,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.jboss.netty.util.HashedWheelTimer;
@@ -55,8 +49,6 @@ public abstract class ConsumerModel {
     return NUM_RETRIES;
   }
 
-//  private final AtomicLong offset = new AtomicLong();
-
   @Autowired private ExecutorService executorService;
 
   @Autowired private ConsumerMessagesRepository consumerMessagesRepository;
@@ -75,56 +67,51 @@ public abstract class ConsumerModel {
   public CompletionStage<Done> save(ConsumerRecord<String, String> record) {
     if (record.topic().equals(getTopic())) {
       executorService.submit(
-              () -> {
-                try {
-                  processMessage(record.value());
-                } catch (Exception e) {
-                  String errorMsg = getStackTrace(e);
-                  processFirstTimeError(record.value(), errorMsg);
-                  log.error("First time error", e);
-                }
-              });
+          () -> {
+            try {
+              processMessage(record.value());
+            } catch (Exception e) {
+              String errorMsg = getStackTrace(e);
+              processFirstTimeError(record.value(), errorMsg);
+              log.error("First time error", e);
+            }
+          });
     } else if (record.topic().equals(getErrorTopic())) {
       executorService.submit(
-              () -> {
-                ConsumerMessages consumerMessages = consumerMessagesRepository.findById(record.value());
-                try {
-                  processMessage(consumerMessages.getMessage());
-                } catch (Exception e) {
-                  String errorMsg = getStackTrace(e);
-                  processError(consumerMessages, errorMsg);
-                  log.error("error", e);
-                }
-              });
+          () -> {
+            ConsumerMessages consumerMessages = consumerMessagesRepository.findById(record.value());
+            try {
+              processMessage(consumerMessages.getMessage());
+            } catch (Exception e) {
+              String errorMsg = getStackTrace(e);
+              processError(consumerMessages, errorMsg);
+              log.error("error", e);
+            }
+          });
     }
-//    offset.set(record.offset());
     return CompletableFuture.completedFuture(Done.getInstance());
   }
-
-//  private CompletionStage<Long> loadOffset() {
-//    return CompletableFuture.completedFuture(offset.get());
-//  }
 
   public abstract void processMessage(String str) throws IOException;
 
   String processError(ConsumerMessages consumerMessage, String errorMsg) {
     log.error(
-            "processing error:"
-                    + consumerMessage.getId()
-                    + (consumerMessage.getRetryCount() + 1L)
-                    + errorMsg);
+        "processing error:"
+            + consumerMessage.getId()
+            + (consumerMessage.getRetryCount() + 1L)
+            + errorMsg);
     if (consumerMessage.getRetryCount() < getNumRetries()) {
       consumerMessage.setLastUpdatedAt(DateTime.now().getMillis());
       consumerMessage.setRetryCount(consumerMessage.getRetryCount() + 1L);
       consumerMessage.setErrorMsg(
-              consumerMessage.getErrorMsg()
-                      + ", Retry number "
-                      + consumerMessage.getRetryCount().toString()
-                      + " "
-                      + errorMsg);
+          consumerMessage.getErrorMsg()
+              + ", Retry number "
+              + consumerMessage.getRetryCount().toString()
+              + " "
+              + errorMsg);
       consumerMessagesRepository.save(consumerMessage);
       ConsumerTimer task =
-              new ConsumerTimer(consumerMessage.getId(), getErrorTopic(), kafkaTemplate);
+          new ConsumerTimer(consumerMessage.getId(), getErrorTopic(), kafkaTemplate);
       timer.newTimeout(task, 5 * (consumerMessage.getRetryCount()), TimeUnit.MINUTES);
     }
     return consumerMessage.getMessage();
@@ -149,30 +136,19 @@ public abstract class ConsumerModel {
   }
 
   public void load(
-          ActorMaterializer materializer, ConsumerSettings<String, String> consumerSettings) {
+      ActorMaterializer materializer, ConsumerSettings<String, String> consumerSettings) {
     Set<String> topics = new HashSet<>();
     topics.add(getTopic());
     topics.add(getErrorTopic());
 
     Consumer.committableSource(consumerSettings, Subscriptions.topics(topics))
-            .mapAsync(
-                    1,
-                    msg ->
-                            save(msg.record())
-                                    .thenApply(done -> msg.committableOffset()))
-            .batch(
-                    20,
-                    ConsumerMessage::createCommittableOffsetBatch,
-                    ConsumerMessage.CommittableOffsetBatch::updated
-            )
-            .mapAsync(3, ConsumerMessage.Committable::commitJavadsl)
-            .to(Sink.ignore())
-            .run(materializer);
-//    this.loadOffset()
-//        .thenAccept(
-//            fromOffset ->
-//                Consumer.plainSource(consumerSettings, Subscriptions.topics(topics))
-//                    .mapAsync(1, this::save)
-//                    .runWith(Sink.ignore(), materializer));
+        .mapAsync(1, msg -> save(msg.record()).thenApply(done -> msg.committableOffset()))
+        .batch(
+            20,
+            ConsumerMessage::createCommittableOffsetBatch,
+            ConsumerMessage.CommittableOffsetBatch::updated)
+        .mapAsync(3, ConsumerMessage.Committable::commitJavadsl)
+        .to(Sink.ignore())
+        .run(materializer);
   }
 }
