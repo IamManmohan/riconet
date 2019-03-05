43a5ba8eef51ae852f2a3bb831e10b6bed485ab4
ashfakh_rivigo <ashfakh.k@rivigo.com>
2019-03-05 11:26:44 +0530

    time out added

--- core/src/main/java/com/rivigo/riconet/core/service/impl/QcModelServiceImpl.java
+++ core/src/main/java/com/rivigo/riconet/core/service/impl/QcModelServiceImpl.java
@@ -2,8 +2,14 @@ package com.rivigo.riconet.core.service.impl;
 
 import com.rivigo.riconet.core.dto.qc.QcRequestDTO;
 import com.rivigo.riconet.core.dto.qc.QcResponseDTO;
+import com.rivigo.riconet.core.enums.ZoomPropertyName;
 import com.rivigo.riconet.core.service.QcApiClientService;
 import com.rivigo.riconet.core.service.QcModelService;
+import com.rivigo.riconet.core.service.ZoomPropertyService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,19 +21,29 @@ public class QcModelServiceImpl implements QcModelService {
 
   @Autowired private QcApiClientService qcApiClientService;
 
+  @Autowired private ExecutorService executorService;
+
+  @Autowired private ZoomPropertyService zoomPropertyService;
+
   @Override
   public Boolean getQcValidationFlag(Long cnId) {
     try {
       log.info("Calling Qc model for Cn Id : {}", cnId);
       QcRequestDTO qcRequestDTO = new QcRequestDTO();
       qcRequestDTO.setConsignmentId(cnId);
-      QcResponseDTO qcResponseDTO = this.qcApiClientService.getQcFlag(qcRequestDTO);
+      Long qcApiTimeOut = zoomPropertyService.getLong(ZoomPropertyName.QC_MODEL_API_TIMEOUT, 1000L);
+      final Future<QcResponseDTO> f =
+          executorService.submit(() -> qcApiClientService.getQcFlag(qcRequestDTO));
+      QcResponseDTO qcResponseDTO = f.get(qcApiTimeOut, TimeUnit.MILLISECONDS);
       log.info(
           "Response from QC model for Cn Id : {}, QC needed : {}, Reason : {}",
           cnId,
           qcResponseDTO.getDecision(),
           qcResponseDTO.getDisposition());
       return qcResponseDTO.getDecision();
+    } catch (TimeoutException e) {
+      log.error("QC model API Call Timed out for {}, Exception :{}", cnId, e);
+      return Boolean.FALSE;
     } catch (Exception e) {
       log.error("Error calling QC model API {}", e);
       return Boolean.FALSE;
