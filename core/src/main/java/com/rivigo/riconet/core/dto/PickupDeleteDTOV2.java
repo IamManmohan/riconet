e4d88185d6adb43b2a282a2f03db0088bb4a3a57
Vamsi <vamsi.hemadribhotla@rivigo.com>
2019-05-16 16:20:04 +0530

    ZM-32 pickup auto cancellation

---
+++ core/src/main/java/com/rivigo/riconet/core/dto/PickupDeleteDTOV2.java
@@ -0,0 +1,19 @@
+package com.rivigo.riconet.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class PickupDeleteDTOV2 {
+  Long id;
+  Long failureReasonId;
+  Long parentPRQId; // recurring prq  if null then its not a child prq
+  Boolean deleteParentPRQ; // its original recurring prq
+}
--- core/src/main/java/com/rivigo/riconet/core/service/impl/ExpressAppPickupServiceImpl.java
+++ core/src/main/java/com/rivigo/riconet/core/service/impl/ExpressAppPickupServiceImpl.java
@@ -2,9 +2,18 @@ package com.rivigo.riconet.core.service.impl;
 
 import com.rivigo.riconet.core.enums.KairosMessageFieldNames;
 import com.rivigo.riconet.core.service.ExpressAppPickupService;
+import com.rivigo.riconet.core.service.ZoomBackendAPIClientService;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class ExpressAppPickupServiceImpl implements ExpressAppPickupService {
+
+  @Autowired private ZoomBackendAPIClientService zoomBackendAPIClientService;
+
   @Override
   public void processExpressPickupAutoCancellationEvent(Map<String, String> map) {
     String metadata = map.get(KairosMessageFieldNames.METADATA.name());
