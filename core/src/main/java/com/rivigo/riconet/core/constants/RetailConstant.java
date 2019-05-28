735f59b4bd9b511a8c97c81c2e2458e4ff8496de
ashfakh_rivigo <ashfakh.k@rivigo.com>
2019-05-28 16:12:44 +0530

    ZT-3928

---
+++ core/src/main/java/com/rivigo/riconet/core/constants/RetailConstant.java
@@ -0,0 +1,12 @@
+package com.rivigo.riconet.core.constants;
+
+/** Created by ashfakh on 28/05/19. */
+public class RetailConstant {
+
+  private RetailConstant() {
+    throw new IllegalStateException("Constants class");
+  }
+
+  public static final String BOXWISE_RETAIL_TYPE = "BOX";
+  public static final String BIKE_RETAIL_TYPE = "BIKE";
+}
