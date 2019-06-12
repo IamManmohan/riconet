00fb119fb719a6f77d275e714ddd86cdf41d8b16
ashfakh_rivigo <ashfakh.k@rivigo.com>
2019-06-12 15:43:54 +0530

    bug fix

--- core/src/main/java/com/rivigo/riconet/core/service/impl/PaymentDetailV2ServiceImpl.java
+++ core/src/main/java/com/rivigo/riconet/core/service/impl/PaymentDetailV2ServiceImpl.java
@@ -1,9 +1,14 @@
 package com.rivigo.riconet.core.service.impl;
 
 import com.rivigo.riconet.core.service.PaymentDetailV2Service;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /** Created by ashfakh on 28/05/19. */
+
+@Slf4j
+@Service
 public class PaymentDetailV2ServiceImpl implements PaymentDetailV2Service {
 
   @Autowired private PaymentDetailV2Service paymentDetailV2Service;
