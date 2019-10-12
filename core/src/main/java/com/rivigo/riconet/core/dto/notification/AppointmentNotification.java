bb9c0039266ae312c3f635f8c036bb894cb8649b
Abhishek Tripathi <abhishek.tripathi@rivigo.com>
2019-10-12 16:37:51 +0530

    T1846 removing unused varaibles and adding toString

--- core/src/main/java/com/rivigo/riconet/core/dto/notification/AppointmentNotification.java
+++ core/src/main/java/com/rivigo/riconet/core/dto/notification/AppointmentNotification.java
@@ -3,17 +3,17 @@ package com.rivigo.riconet.core.dto.notification;
 import com.rivigo.zoom.common.dto.LocationBasicDTO;
 import com.rivigo.zoom.common.dto.UserBasicDTO;
 import com.rivigo.zoom.common.pojo.AbstractMailNotificationEntity;
-import java.util.HashSet;
-import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(callSuper = true)
 public class AppointmentNotification extends AbstractMailNotificationEntity {
 
   private String id;
@@ -21,7 +21,4 @@ public class AppointmentNotification extends AbstractMailNotificationEntity {
   private Long consignmentId;
   private UserBasicDTO responsiblePerson;
   private LocationBasicDTO responsibleLocation;
-  private Set<String> emailIdList = new HashSet<>();
-  private Set<String> ccList = new HashSet<>();
-  private Set<String> bccList = new HashSet<>();
 }
--- core/src/main/java/com/rivigo/riconet/core/dto/notification/PickupNotification.java
+++ core/src/main/java/com/rivigo/riconet/core/dto/notification/PickupNotification.java
@@ -8,11 +8,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString(callSuper = true)
 public class PickupNotification {
 
   private String id;
