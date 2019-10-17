9dda1a7086c44d74d04a5ed93edb1b19ad95e200
Palash Goel <palash.goel@rivigo.com>
2019-10-17 14:12:37 +0530

    T664 Formatting

--- core/src/main/java/com/rivigo/riconet/core/service/impl/ConsignmentReadOnlyServiceImpl.java
+++ core/src/main/java/com/rivigo/riconet/core/service/impl/ConsignmentReadOnlyServiceImpl.java
@@ -3,10 +3,9 @@ package com.rivigo.riconet.core.service.impl;
 import com.rivigo.riconet.core.service.ConsignmentReadOnlyService;
 import com.rivigo.zoom.common.model.ConsignmentReadOnly;
 import com.rivigo.zoom.common.repository.mysql.ConsignmentReadOnlyRepository;
+import com.rivigo.zoom.exceptions.ZoomException;
 import java.util.List;
 import java.util.Optional;
-
-import com.rivigo.zoom.exceptions.ZoomException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
