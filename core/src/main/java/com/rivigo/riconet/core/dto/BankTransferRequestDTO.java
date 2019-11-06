bd383f978a31de2fb4e4cb685389e3ad67083e52
Palash Goel <palash.goel@rivigo.com>
2019-11-06 18:00:18 +0530

    T664 Adding NoArgsConstructor

--- core/src/main/java/com/rivigo/riconet/core/dto/BankTransferRequestDTO.java
+++ core/src/main/java/com/rivigo/riconet/core/dto/BankTransferRequestDTO.java
@@ -3,10 +3,12 @@ package com.rivigo.riconet.core.dto;
 import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BankTransferRequestDTO {
 
   @NonNull @NotBlank private String transactionReferenceNo;
