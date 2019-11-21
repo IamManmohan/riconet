8d5d4326a24493c5140af763d20962e0c9f75b96
Siddharth Kumar <siddharth.kumar@rivigo.com>
2019-11-21 15:10:51 +0530

    T2272 added msg listener in redis

--- core/src/main/java/com/rivigo/riconet/core/config/RiconetRedisConfig.java
+++ core/src/main/java/com/rivigo/riconet/core/config/RiconetRedisConfig.java
@@ -2,14 +2,21 @@ package com.rivigo.riconet.core.config;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rivigo.zoom.common.config.RedisMessageSubscriber;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import redis.clients.jedis.JedisPoolConfig;
 
 @Slf4j
@@ -29,7 +36,12 @@ public class RiconetRedisConfig {
   @Value("${redis.maxpoolsize}")
   private Integer redisMaxPoolSize;
 
+  @Getter
+  @Value("${redis.pub.sub.topic:rivigo-cache}")
+  private String redisPubSubTopic;
+
   @Autowired private ObjectMapper objectMapper;
+  @Autowired private AutowireCapableBeanFactory beanFactory;
 
   @Bean
   RedisConnectionFactory redisConnectionFactory() {
@@ -54,5 +66,30 @@ public class RiconetRedisConfig {
       e.printStackTrace();
     }
     return connectionFactory;
+  }
+
+  @Bean
+  MessageListenerAdapter messageListener() {
+    MessageListener listener = new RedisMessageSubscriber();
+    beanFactory.autowireBean(listener);
+    return new MessageListenerAdapter(listener);
+  }
+
+  /**
+   * https://www.baeldung.com/spring-data-redis-pub-sub
+   *
+   * <p>RedisMessageListenerContainer is a class provided by Spring Data Redis which provides
+   * asynchronous behavior for Redis message listeners. This is called internally and, according to
+   * the Spring Data Redis documentation – “handles the low level details of listening, converting
+   * and message dispatching.”
+   *
+   * @return
+   */
+  @Bean
+  RedisMessageListenerContainer redisContainer() {
+    final RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+    container.setConnectionFactory(redisConnectionFactory());
+    container.addMessageListener(messageListener(), new ChannelTopic(redisPubSubTopic));
+    return container;
   }
 }
