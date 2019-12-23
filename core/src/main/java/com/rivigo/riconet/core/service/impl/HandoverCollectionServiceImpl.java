f4d2da2ccf40363595c3f84db0b5f91496473e03
Sahil Soni <sahil.soni@rivigo.com>
2019-12-23 18:31:00 +0530

    [T1289] CR Changes and minor refactor
    Signed-off-by: Sahil Soni <sahil.soni@rivigo.com>

--- core/src/main/java/com/rivigo/riconet/core/service/impl/HandoverCollectionServiceImpl.java
+++ core/src/main/java/com/rivigo/riconet/core/service/impl/HandoverCollectionServiceImpl.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rivigo.collections.api.dto.HandoverCollectionEventPayload;
 import com.rivigo.collections.api.dto.HandoverCollectionExcludeEventPayload;
+import com.rivigo.collections.api.enums.PaymentType.ZoomPaymentType;
 import com.rivigo.finance.utils.TimeUUID;
 import com.rivigo.finance.zoom.enums.ZoomEventType;
-import com.rivigo.riconet.core.constants.HandoverConstant;
 import com.rivigo.riconet.core.dto.ChequeBounceDTO;
 import com.rivigo.riconet.core.service.ConsignmentReadOnlyService;
 import com.rivigo.riconet.core.service.DepositSlipService;
@@ -27,7 +27,6 @@ import com.rivigo.zoom.common.model.depositslip.DepositSlip;
 import com.rivigo.zoom.common.model.neo4j.Location;
 import com.rivigo.zoom.common.repository.mysql.depositslip.ConsignmentDepositSlipRepository;
 import com.rivigo.zoom.exceptions.ZoomException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -64,31 +63,32 @@ public class HandoverCollectionServiceImpl implements HandoverCollectionService 
 
   private final ConsignmentReadOnlyService consignmentReadOnlyService;
 
+  /**
+   * Parse this payload to HandoverCollectionEventPayload, Get location dto for the location code,
+   * create ZoomBookTransactionRequestDTO and hit the zoombook for creating transaction This
+   * transaction is enough for knockingoff/adding amount to OU Book.
+   *
+   * @param payload the event payload from compass collections service
+   * @param eventType the event type(post/unpost)
+   */
   @Override
   public void handleHandoverCollectionPostUnpostEvent(String payload, ZoomEventType eventType) {
-    // Parse this payload to HandoverCollectionEventPayload,
-    // Get location dto for the location code,
-    // create ZoomBookTransactionRequestDTO and hit the zoombook for creating transaction
-    HandoverCollectionEventPayload handoverCollectionEventPayload = null;
-    try {
-      handoverCollectionEventPayload =
-          objectMapper.readValue(payload, HandoverCollectionEventPayload.class);
-    } catch (IOException e) {
-      log.error("Failed to parse HandoverCollectionEventPayload - {}", e.getLocalizedMessage());
-      throw new ZoomException("Failed to create DTO from payload - %s", payload);
-    }
+    log.info("Handling post/unpost event : {}", payload);
+    HandoverCollectionEventPayload handoverCollectionEventPayload =
+        getDtoFromPayload(payload, HandoverCollectionEventPayload.class);
 
     // fill transactionRequestDTO
     ZoomBookTransactionRequestDTO transactionRequestDTO =
         ZoomBookTransactionRequestDTO.builder()
-            .transactionType(getTransactionType(handoverCollectionEventPayload, eventType))
+            .transactionType(getTransactionType(eventType))
             .clientRequestId(getTimeUUID())
             .tenantType(ZoomBookTenantType.RETAIL)
             .functionType(ZoomBookFunctionType.OU_OUTSTANDING)
             .orgId(getLocation(handoverCollectionEventPayload.getLocationCode()).getId())
             .amount(handoverCollectionEventPayload.getCollectionAmount())
             .transactionHeader(
-                getTransactionHeader(handoverCollectionEventPayload)) // (CASH OR CHEQUE)
+                getTransactionHeader(
+                    handoverCollectionEventPayload.getPaymentType())) // (CASH OR CHEQUE)
             .transactionSubHeader(ZoomBookTransactionSubHeader.KNOCKOFF)
             .remarks(payload)
             // .notification() //not needed
@@ -100,8 +100,7 @@ public class HandoverCollectionServiceImpl implements HandoverCollectionService 
         Collections.singletonList(transactionRequestDTO));
   }
 
-  private ZoomBookTransactionType getTransactionType(
-      HandoverCollectionEventPayload handoverCollectionEventPayload, ZoomEventType eventType) {
+  private ZoomBookTransactionType getTransactionType(ZoomEventType eventType) {
     switch (eventType) {
       case HANDOVER_COLLECTION_UNPOST:
         return ZoomBookTransactionType.DEBIT;
@@ -112,26 +111,20 @@ public class HandoverCollectionServiceImpl implements HandoverCollectionService 
     }
   }
 
+  /**
+   * Parse this payload to HandoverCollectionExcludeEventPayload, This is cheque bounce, and need to
+   * be handled as such, crete credit+debit entries to OU_OUTSTANDING book for the OU, and hit the
+   * recoveryPending api for the CNs who's cheque is bounced.
+   *
+   * @param payload the event payload from compass collections service
+   * @param eventType the event type(EXCLUDE)
+   */
   @Override
   public void handleHandoverCollectionExcludeEvent(String payload, ZoomEventType eventType) {
-    // This is cheque bounce, and need to be handled as such
+    log.info("Handling Cheque Bounce Event from collections service : {}", payload);
 
-    // Parse this payload to HandoverCollectionEventPayload,
-    // Get location dto for the location code,
-    // create ZoomBookTransactionRequestDTO and hit the zoombook for creating transaction
-    log.info("Handling Cheque Bounce Event from collections service");
-    List<ZoomBookTransactionRequestDTO> transactionRequestDTOList = new ArrayList<>();
-    ZoomBookTransactionRequestDTO transactionRequestDTO;
-
-    HandoverCollectionExcludeEventPayload handoverExcludePayload;
-    try {
-      handoverExcludePayload =
-          objectMapper.readValue(payload, HandoverCollectionExcludeEventPayload.class);
-    } catch (IOException e) {
-      log.error(
-          "Failed to parse HandoverCollectionExcludeEventPayload - {}", e.getLocalizedMessage());
-      throw new ZoomException("Failed to create DTO from payload - %s", payload);
-    }
+    HandoverCollectionExcludeEventPayload handoverExcludePayload =
+        getDtoFromPayload(payload, HandoverCollectionExcludeEventPayload.class);
 
     DepositSlip depositSlip =
         depositSlipService
@@ -161,14 +154,8 @@ public class HandoverCollectionServiceImpl implements HandoverCollectionService 
       return;
     }
 
-    // 2 entries, one credit, and one debit for cheque bounce
-    transactionRequestDTO = getNewTransactionRequestDTOForChequeBounce(handoverExcludePayload);
-    transactionRequestDTO.setTransactionType(ZoomBookTransactionType.DEBIT);
-    transactionRequestDTOList.add(transactionRequestDTO);
-    transactionRequestDTO = SerializationUtils.clone(transactionRequestDTO);
-    transactionRequestDTO.setTransactionType(ZoomBookTransactionType.CREDIT);
-    transactionRequestDTO.setClientRequestId(getTimeUUID());
-    transactionRequestDTOList.add(transactionRequestDTO);
+    List<ZoomBookTransactionRequestDTO> transactionRequestDTOList =
+        getTransactionRequestsForChequeBounce(handoverExcludePayload);
 
     log.info(
         "Making zoomBook API call for creating transactions, transactionsList: {}",
@@ -201,28 +188,45 @@ public class HandoverCollectionServiceImpl implements HandoverCollectionService 
     return locationService.getLocationByCode(code);
   }
 
-  private ZoomBookTransactionRequestDTO getNewTransactionRequestDTOForChequeBounce(
+  /**
+   * Create DTOs for credit and debit, Get location dto for the location code, (dont forget to keep
+   * the clientRequestId unique)
+   *
+   * @param payload the payload from collection for exclude event.
+   * @return List of 2 entries, one credit, and one debit for cheque bounce
+   */
+  private List<ZoomBookTransactionRequestDTO> getTransactionRequestsForChequeBounce(
       HandoverCollectionExcludeEventPayload payload) {
-    return ZoomBookTransactionRequestDTO.builder()
-        .clientRequestId(getTimeUUID())
-        .tenantType(ZoomBookTenantType.RETAIL)
-        .functionType(ZoomBookFunctionType.OU_OUTSTANDING)
-        .orgId(getLocation(payload.getLocationCode()).getId())
-        .amount(payload.getCollectionAmount())
-        .transactionHeader(ZoomBookTransactionHeader.CHEQUE)
-        .transactionSubHeader(ZoomBookTransactionSubHeader.BOUNCED)
-        .remarks(
-            Optional.ofNullable(payload.getRemarks())
-                .orElse("")
-                .replaceAll("[^a-zA-Z0-9,./-]", " "))
-        .effectedAt(payload.getCollectionPostingDate())
-        .reference(
-            payload.getDepositSlipId()
-                + "|"
-                + payload.getBankName()
-                + "|"
-                + payload.getChequeNumber())
-        .build();
+    List<ZoomBookTransactionRequestDTO> transactionRequestDTOList = new ArrayList<>();
+    ZoomBookTransactionRequestDTO onePayload =
+        ZoomBookTransactionRequestDTO.builder()
+            .clientRequestId(getTimeUUID())
+            .tenantType(ZoomBookTenantType.RETAIL)
+            .functionType(ZoomBookFunctionType.OU_OUTSTANDING)
+            .orgId(getLocation(payload.getLocationCode()).getId())
+            .amount(payload.getCollectionAmount())
+            .transactionHeader(ZoomBookTransactionHeader.CHEQUE)
+            .transactionSubHeader(ZoomBookTransactionSubHeader.BOUNCED)
+            .remarks(
+                Optional.ofNullable(payload.getRemarks())
+                    .orElse("")
+                    .replaceAll("[^a-zA-Z0-9,./-]", " "))
+            .effectedAt(payload.getCollectionPostingDate())
+            .reference(
+                payload.getDepositSlipId()
+                    + "|"
+                    + payload.getBankName()
+                    + "|"
+                    + payload.getChequeNumber()
+                    + "|"
+                    + payload.getCollectionId())
+            .build();
+    onePayload.setTransactionType(ZoomBookTransactionType.DEBIT);
+    transactionRequestDTOList.add(onePayload);
+    onePayload = SerializationUtils.clone(onePayload);
+    onePayload.setTransactionType(ZoomBookTransactionType.CREDIT);
+    onePayload.setClientRequestId(getTimeUUID());
+    return transactionRequestDTOList;
   }
 
   // WARNING: MUTATOR : Fills up the paymentDetailsV2List as well
@@ -239,7 +243,8 @@ public class HandoverCollectionServiceImpl implements HandoverCollectionService 
           if (paymentDetail != null
               && Objects.equals(chequeNumber, paymentDetail.getTransactionReferenceNo())
               && Objects.equals(bankName, paymentDetail.getBankName())
-              && !HandoverConstant.pendingStatuses.contains(paymentDetail.getHandoverStatus())) {
+              && paymentDetail.getHandoverStatus() != null
+              && !paymentDetail.getHandoverStatus().getIsPending()) {
             cnIdToPaymentDetailV2Map.put(cnId, paymentDetail);
           }
         });
@@ -251,13 +256,12 @@ public class HandoverCollectionServiceImpl implements HandoverCollectionService 
         .collect(Collectors.toMap(ConsignmentReadOnly::getId, cn -> cn));
   }
 
-  private ZoomBookTransactionHeader getTransactionHeader(
-      HandoverCollectionEventPayload handoverCollectionEventPayload) {
+  private ZoomBookTransactionHeader getTransactionHeader(ZoomPaymentType paymentType) {
     ZoomBookTransactionHeader transactionHeader = null;
-    if (handoverCollectionEventPayload.getPaymentType() == null) {
-      throw new ZoomException("Invalid PaymentType received, %s", handoverCollectionEventPayload);
+    if (paymentType == null) {
+      throw new ZoomException("NULL PaymentType received.");
     }
-    switch (handoverCollectionEventPayload.getPaymentType()) {
+    switch (paymentType) {
       case CHEQUE:
         transactionHeader = ZoomBookTransactionHeader.CHEQUE;
         break;
@@ -267,7 +271,7 @@ public class HandoverCollectionServiceImpl implements HandoverCollectionService 
       default:
         // Currently it can either be cash or cheque, so lets just keep them at that.
         // Online knockoffs are not supposed to come from this event(compass collection service).
-        throw new ZoomException("Invalid PaymentType received, %s", handoverCollectionEventPayload);
+        throw new ZoomException("Invalid PaymentType received, %s", paymentType);
     }
     return transactionHeader;
   }
@@ -283,5 +287,15 @@ public class HandoverCollectionServiceImpl implements HandoverCollectionService 
         chequeBounceDTOList,
         jsonNode);
     // Does this make a difference what happens to the request?
+  }
+
+  private <T> T getDtoFromPayload(String payload, Class<T> clazz) {
+    try {
+      return objectMapper.readValue(payload, clazz);
+    } catch (Exception e) {
+      log.error("Failed to parse {} - {}", clazz.toString(), e.getLocalizedMessage());
+      throw new ZoomException(
+          "Failed to create %s DTO from payload - %s", clazz.toString(), payload);
+    }
   }
 }
