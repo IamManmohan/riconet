fff528c7955f17d6432219f86507c625ed470862
Nikhil Rawat <nikhil.rawat@rivigo.com>
2020-03-12 11:28:10 +0530

    T4119 POD Dispatch - spotlessApply

--- core/src/main/java/com/rivigo/riconet/core/service/LocationService.java
+++ core/src/main/java/com/rivigo/riconet/core/service/LocationService.java
@@ -1,10 +1,9 @@
 package com.rivigo.riconet.core.service;
 
 import com.rivigo.zoom.common.model.neo4j.Location;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Map;
+import org.springframework.stereotype.Service;
 
 @Service
 public interface LocationService {
