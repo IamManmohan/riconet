cec4d9f7fd4a773322c671b80f4f80d899bfb9e9
Jatin Malik <jatin.malik@rivigo.com>
2020-05-14 01:30:48 +0530

    T5450 set captain code

--- core/src/main/java/com/rivigo/riconet/core/service/TransportationPartnerMappingService.java
+++ core/src/main/java/com/rivigo/riconet/core/service/TransportationPartnerMappingService.java
@@ -1,10 +1,14 @@
 package com.rivigo.riconet.core.service;
 
 import com.rivigo.zoom.common.model.TransportationPartnerMapping;
+import java.util.List;
+import java.util.Map;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface TransportationPartnerMappingService {
 
   TransportationPartnerMapping getByDRSId(Long drsId);
+
+  Map<Long, Long> getUserIdByDrsId(List<Long> drsIds);
 }
--- core/src/main/java/com/rivigo/riconet/core/service/impl/TransportationPartnerMappingServiceImpl.java
+++ core/src/main/java/com/rivigo/riconet/core/service/impl/TransportationPartnerMappingServiceImpl.java
@@ -4,6 +4,9 @@ import com.rivigo.riconet.core.service.TransportationPartnerMappingService;
 import com.rivigo.zoom.common.enums.ZoomTripType;
 import com.rivigo.zoom.common.model.TransportationPartnerMapping;
 import com.rivigo.zoom.common.repository.mysql.TransportationPartnerMappingRepository;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +20,16 @@ public class TransportationPartnerMappingServiceImpl
   public TransportationPartnerMapping getByDRSId(Long drsId) {
     return transportationPartnerMappingRepository.findByTransportationTypeAndTransportationId(
         ZoomTripType.DRS, drsId);
+  }
+
+  @Override
+  public Map<Long, Long> getUserIdByDrsId(List<Long> drsIds) {
+    return transportationPartnerMappingRepository
+        .findByTransportationTypeAndTransportationIdIn(ZoomTripType.DRS, drsIds)
+        .stream()
+        .collect(
+            Collectors.toMap(
+                TransportationPartnerMapping::getTransportationId,
+                TransportationPartnerMapping::getUserId));
   }
 }
