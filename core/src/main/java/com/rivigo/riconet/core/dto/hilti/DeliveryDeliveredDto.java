77217b1034c17e6e7ef510d6bc073801623d7383
Madhav Agrawal <madhav.agrawal@rivigo.com>
2020-06-01 12:44:43 +0530

    T4905 dto changes done

--- core/src/main/java/com/rivigo/riconet/core/dto/hilti/DeliveryDeliveredDto.java
+++ core/src/main/java/com/rivigo/riconet/core/dto/hilti/DeliveryDeliveredDto.java
@@ -3,18 +3,14 @@ package com.rivigo.riconet.core.dto.hilti;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@SuperBuilder
 @ToString
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
