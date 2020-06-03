094ae20d5ee6c790377e555b7380f16500ddc6b8
Madhav Agrawal <madhav.agrawal@rivigo.com>
2020-06-03 12:37:29 +0530

    T5450 reverting one chag

--- core/src/main/java/com/rivigo/riconet/core/config/ZoomBackendDatabaseConfig.java
+++ core/src/main/java/com/rivigo/riconet/core/config/ZoomBackendDatabaseConfig.java
@@ -26,7 +26,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories(
-  basePackages = "com.rivigo.zoom.common.repository",
+  basePackages = "com.rivigo.zoom.common.repository.mysql",
   entityManagerFactoryRef = "entityManagerFactory",
   transactionManagerRef = "zoomTransactionManager"
 )
