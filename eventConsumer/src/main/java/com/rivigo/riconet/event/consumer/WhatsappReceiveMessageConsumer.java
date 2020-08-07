3a2bcbb56ba133390532e38e36a8d96ef9a36b2f
Komal Agarwal <komal.agarwal@rivigo.com>
2020-08-07 15:25:33 +0530

    T00 disable whatsapp for staging

--- eventConsumer/src/main/java/com/rivigo/riconet/event/consumer/WhatsappReceiveMessageConsumer.java
+++ eventConsumer/src/main/java/com/rivigo/riconet/event/consumer/WhatsappReceiveMessageConsumer.java
@@ -2,14 +2,19 @@ package com.rivigo.riconet.event.consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rivigo.riconet.core.consumerabstract.ConsumerModel;
+import com.rivigo.riconet.core.predicates.EnvironmentPredicate;
+import com.rivigo.riconet.core.service.ZoomPropertyService;
 import com.rivigo.riconet.event.config.EventTopicNameConfig;
 import com.rivigo.riconet.event.dto.whatsappbot.BasePubSubDto;
 import com.rivigo.riconet.event.dto.whatsappbot.ReceiveWhatsappMessageDto;
 import com.rivigo.riconet.event.service.WhatsappBotService;
 import java.io.IOException;
+
+import com.rivigo.zoom.common.enums.ZoomPropertyName;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -23,6 +28,8 @@ public class WhatsappReceiveMessageConsumer extends ConsumerModel {
 
   private final ObjectMapper objectMapper;
 
+  private final ZoomPropertyService zoomPropertyService;
+
   @Override
   public String getTopic() {
     return eventTopicNameConfig.getWhatsappMessageReceive();
@@ -34,9 +41,14 @@ public class WhatsappReceiveMessageConsumer extends ConsumerModel {
   }
 
   public void processMessage(String str) throws IOException {
-    BasePubSubDto basePubSubDto = objectMapper.readValue(str, BasePubSubDto.class);
-    ReceiveWhatsappMessageDto receiveWhatsappMessageDto =
-        objectMapper.readValue(basePubSubDto.getMessage(), ReceiveWhatsappMessageDto.class);
-    whatsappBotService.processReceivedWhatsappMessage(receiveWhatsappMessageDto);
+    boolean isEmailEnabled =
+            zoomPropertyService.getBoolean(ZoomPropertyName.RECEIVE_WHATSAPP_MESSAGE_ENABLED, false);
+    if(EnvironmentPredicate.isActiveSpringProfileProduction().test(System.getProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME))||isEmailEnabled) {
+      BasePubSubDto basePubSubDto = objectMapper.readValue(str, BasePubSubDto.class);
+      ReceiveWhatsappMessageDto receiveWhatsappMessageDto =
+              objectMapper.readValue(basePubSubDto.getMessage(), ReceiveWhatsappMessageDto.class);
+
+      whatsappBotService.processReceivedWhatsappMessage(receiveWhatsappMessageDto);
+    }
   }
 }
