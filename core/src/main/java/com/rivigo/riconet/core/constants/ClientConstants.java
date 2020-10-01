7b93529abef4f9600231ec3b30c0801cec11d35e
mohak <mohak.garg@rivigo.com>
2020-10-01 16:02:14 +0530

    T5479 add/update vas only for forward cn movement type

--- core/src/main/java/com/rivigo/riconet/core/constants/ClientConstants.java
+++ core/src/main/java/com/rivigo/riconet/core/constants/ClientConstants.java
@@ -14,4 +14,6 @@ public class ClientConstants {
   public static final String FLIPKART_INDIA_ZOOM_CLIENT = "4128";
   public static final String FLIPKART_INTERNET_SELLER_CLIENT = "5522";
   public static final String ZOOM_DOCS_CONSIGNMENT_CLIENT_CODE = "ZDOCS";
+
+  public static final String CORPORATE_CLIENT_RETURN_CN_MOVEMENT_TYPE = "RETURN";
 }
