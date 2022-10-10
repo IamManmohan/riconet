e74e4572bd78ebce388cd7f7b5d92fc970911441
Sunny Jain <sunny.jain@rivigo.com>
2022-10-10 17:03:44 +0530

    T31772-fix1 --> develop [riconet] : T31772 Fix tests
    Reviewers: krishna
    
    Reviewed By: krishna
    
    Maniphest Tasks: T31772
    
    Differential Revision: http://phabricator.rivigo.com/D8882

--- core/src/test/java/com/rivigo/riconet/core/test/TesterBase.java
+++ core/src/test/java/com/rivigo/riconet/core/test/TesterBase.java
@@ -2,10 +2,7 @@ package com.rivigo.riconet.core.test;
 
 import com.rivigo.riconet.core.config.AsyncConfig;
 import com.rivigo.riconet.core.config.KafkaConfig;
-import com.rivigo.riconet.core.config.RiconetRedisConfig;
 import com.rivigo.riconet.core.config.ServiceConfig;
-import com.rivigo.riconet.core.config.ZoomBackendDatabaseConfig;
-import com.rivigo.riconet.core.config.ZoomBackendNeo4jReadConfig;
 import com.rivigo.riconet.core.config.ZoomRiconetConfig;
 import com.rivigo.zoom.util.commons.config.SerDeConfig;
 import org.junit.Test;
@@ -14,16 +11,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/** author: Sunny Jain */
 @RunWith(SpringRunner.class)
 @ContextConfiguration(
   classes = {
     AsyncConfig.class,
     KafkaConfig.class,
-    RiconetRedisConfig.class,
     ServiceConfig.class,
     ZoomRiconetConfig.class,
-    ZoomBackendNeo4jReadConfig.class,
-    ZoomBackendDatabaseConfig.class,
     com.rivigo.zoom.util.commons.config.AsyncConfig.class,
     SerDeConfig.class
   }
