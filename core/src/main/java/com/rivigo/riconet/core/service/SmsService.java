738528968866037009c994374fbbb1f1ce6873a8
rohittomar2 <rohit.tomar@rivigo.com>
2023-10-27 16:55:11 +0530

    T33865 --> develop [riconet] : T33865 fixes for remaining message types
    Reviewers: sunny_jain
    
    Reviewed By: sunny_jain
    
    Maniphest Tasks: T33865
    
    Differential Revision: http://phabricator.rivigo.com/D10783

--- core/src/main/java/com/rivigo/riconet/core/service/SmsService.java
+++ core/src/main/java/com/rivigo/riconet/core/service/SmsService.java
@@ -3,8 +3,6 @@ package com.rivigo.riconet.core.service;
 import com.rivigo.riconet.core.dto.TemplateV2DTO;
 import java.util.Map;
 
-import java.util.Map;
-
 public interface SmsService {
 
   String sendSms(String mobileNo, String message);
