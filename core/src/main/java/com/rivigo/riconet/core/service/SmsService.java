9df4c05cf62bc90bfb3faa97e883df041cc738bd
Jayant Aggarwal <jayant.aggarwal@rivigo.com>
2023-10-27 16:24:26 +0530

    T33865 --> develop [riconet] : T33865 fixes for remaining message types
    Reviewers: sunny_jain
    
    Reviewed By: sunny_jain
    
    Maniphest Tasks: T33865
    
    Differential Revision: http://phabricator.rivigo.com/D10759

--- core/src/main/java/com/rivigo/riconet/core/service/SmsService.java
+++ core/src/main/java/com/rivigo/riconet/core/service/SmsService.java
@@ -2,9 +2,13 @@ package com.rivigo.riconet.core.service;
 
 import com.rivigo.riconet.core.dto.TemplateV2DTO;
 
+import java.util.Map;
+
 public interface SmsService {
 
   String sendSms(String mobileNo, String message);
 
   Boolean sendSmsV2(String mobileNo, TemplateV2DTO template);
+
+  Map<String, String> sanitizeStringValuesForStringLimit(Map<String,String> valuesMap);
 }
